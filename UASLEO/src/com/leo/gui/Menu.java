b25dfcb50856184ee4c667d0465f1c819fdceead
elkriyansah <154106123+elkriyansah@users.noreply.github.com>
2024-07-24 23:51:07 +0800

    UAS

---
+++ UASLEO/src/com/leo/gui/Menu.java
@@ -0,0 +1,22 @@
+package com.leo.gui;
+
+public class Menu {
+    private String Menu;
+    private double harga;
+
+    public String getMenu() {
+        return Menu;
+    }
+
+    public void setMenu(String Menu) {
+        this.Menu = Menu;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+}
